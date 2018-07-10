f9dcd2071ac7a65b39efa0cca28147be078b12aa
mettlife <mettlifecgn@gmail.com>
2018-07-10 21:37:30 +0200

    Trump gives
    First try on trump gifs

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
+const api = 'qPcMqnjC9YxHgog5G8VBSW1FQW9gSbLf';
 
 client.on('ready',() => {
     console.log('I\'m online\nI\'m online');
-})
+});
 
 var prefix ="$"
+
 client.on('message', message => {
     if (message.author === client.user) return;
-        if (message.content.startsWith(prefix + 'ping')) {
+    if (message.content.startsWith(prefix + 'ping')) {
           //(message.content === 'ping') will only respond to "ping" in chat
           //message.content.startsWith('ping') will reply to any message that starts with "ping"
-        message.channel.sendMessage('pong');
-        }
+    message.channel.sendMessage('pong');
+    }
           //message.reply responds in channel with @username
           //message.channel.sendMessage responds in channel with regular message
-        if (message.content.startsWith(prefix + 'brommi')) {
-        message.channel.sendMessage('Wat ein noob!');
-        }
+    if (message.content.startsWith(prefix + 'brommi')) {
+    message.channel.sendMessage('Wat ein noob!');
+    }
+});
+
+client.on('message', message => {
+  if (message.author === client.user) return;
+  if(message.content.startsWith(config.prefix)) {
+    let tag = 'trump'
+    if giphy.random('gifs', {tag})
+        .then((res) => {
+          // console.log(res);
+          message.channel.send(res.data.url);
+        })
+  }
 });
 
 client.login(settings.token);
