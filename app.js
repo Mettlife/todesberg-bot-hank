6c3d2e66e3a069f2fc1caf02fb796d65ede329d1
mettlife <mettlifecgn@gmail.com>
2018-07-10 21:10:48 +0200

    Commands added
    - added brommi command

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ client.on('ready',() => {
 var prefix ="$"
 client.on('message', message => {
     if (message.author === client.user) return;
-    if (message.content.startsWith(prefix + 'ping')) {
-        //(message.content === 'ping') will only respond to "ping" in chat
-        //message.content.startsWith('ping') will reply to any message that starts with "ping"
+        if (message.content.startsWith(prefix + 'ping')) {
+          //(message.content === 'ping') will only respond to "ping" in chat
+          //message.content.startsWith('ping') will reply to any message that starts with "ping"
         message.channel.sendMessage('pong');
-        //message.reply responds in channel with @username
-        //message.channel.sendMessage responds in channel with regular message
+          //message.reply responds in channel with @username
+          //message.channel.sendMessage responds in channel with regular message
+        if (message.content.startsWith(prefix + 'brommi')) {
+        message.channel.sendMessage('Wat ein noob!')
+        }
     }
 });
 
