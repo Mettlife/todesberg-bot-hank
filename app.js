f0a426a1c0adb614374da36768fdbf694d9fc4d0
Mettlife <mettlifecgn@gmail.com>
2018-07-10 20:59:40 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const settings = require('./settings.json');
+
+client.on('ready',() => {
+    console.log('I\'m online\nI\'m online');
+})
+
+var prefix ="$"
+client.on('message', message => {
+    if (message.author === client.user) return;
+    if (message.content.startsWith(prefix + 'ping')) {
+        //(message.content === 'ping') will only respond to "ping" in chat
+        //message.content.startsWith('ping') will reply to any message that starts with "ping"
+        message.channel.sendMessage('pong');
+        //message.reply responds in channel with @username
+        //message.channel.sendMessage responds in channel with regular message
+    }
+});
+
+client.login(settings.token);
