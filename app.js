ad929588388bd3e9ae2582cf729453260289638a
mettlife <mettlifecgn@gmail.com>
2018-07-10 22:48:42 +0200

    bromnmi 2

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
-const giphy = new Giphy('dc6zaTOxFJmzC');
-const Giphy = require('giphy.js');
 
 client.on('ready',() => {
     console.log('I\'m online\nI\'m online');
 });
 
 var prefix ="$"
-var giphy = require('giphy-api')('dc6zaTOxFJmzC');
 
 client.on('message', message => {
     if (message.author === client.user) return;
@@ -24,15 +21,5 @@ client.on('message', message => {
     message.channel.sendMessage('Wat ein noob!');
     }
 });
-
-/*
-giphy.random({
-  rating: 'r',
-  fmt: 'json',
-  tag: 'trump'
-})
-  .then((response) => console.log(response))
-  .catch((error) => console.error(error));
-*/
 
 client.login(settings.token);
